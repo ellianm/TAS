9674bff8c84457a6f022d734ef18b84e9a2e3671
ellianm <ellianmarcondes@hotmail.com>
2020-11-17 07:23:20 -0300

    commit 17/11/2020

--- tas-server/src/main/java/br/edu/materdei/tas/server/controller/FornecedorController.java
+++ tas-server/src/main/java/br/edu/materdei/tas/server/controller/FornecedorController.java
@@ -4,75 +4,145 @@ import br.edu.materdei.tas.compra.entity.FornecedorEntity;
 import br.edu.materdei.tas.compra.service.FornecedorService;
 import br.edu.materdei.tas.core.exception.ResourceNotFoundException;
 import br.edu.materdei.tas.server.utils.CustomErrorResponse;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@RestController
 public class FornecedorController {
-
+    
     @Autowired
     private FornecedorService service;
-
+    
     @GetMapping("fornecedores")
     public ResponseEntity<List<FornecedorEntity>> findAll() {
-        List<FornecedorEntity> fornecedores = this.service.findAll();
-
-        return new ResponseEntity(fornecedores, HttpStatus.OK);
-    }
-    @GetMapping("fornecedores/{id}")
-    public ResponseEntity findById(@PathVariable("id") Integer id) {
         try {
-            FornecedorEntity fornecedor = this.service.findById(id);
-            return new ResponseEntity(fornecedor, HttpStatus.OK);
-        } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe uma fornecedor com esse código"),HttpStatus.NOT_FOUND);
+            //Busca TODOS os registros no banco de dados
+            List<FornecedorEntity> fornecedores = service.findAll();
+
+            //Retorna a lista de fornecedores
+            return new ResponseEntity(fornecedores, HttpStatus.OK);  
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+            
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    
     @PostMapping("fornecedores")
     public ResponseEntity create(@RequestBody FornecedorEntity fornecedor) {
         try {
-
+            //Insere o fornecedor no bando de dados
             this.service.save(fornecedor);
+            
+            //Retorna o fornecedor inserido
             return new ResponseEntity(fornecedor, HttpStatus.CREATED);
-
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+            
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    @PutMapping("fornecedores/{id}")
-    public ResponseEntity update(@PathVariable("ID") Integer id, @RequestBody FornecedorEntity fornecedor) {
+    
+    @GetMapping("fornecedores/{id}")
+    public ResponseEntity findByID(@PathVariable("id") Integer id) {
         try {
-            FornecedorEntity found = this.service.findById(id);
-            fornecedor.setId(found.getId());
-
-            this.service.save(fornecedor);
-
+            
+            //Verifica se existe um fornecedor com o ID passado por parametro
+            FornecedorEntity fornecedor = this.service.findById(id);
+            
+            //Retorna o fornecedor com o ID do parametro
             return new ResponseEntity(fornecedor, HttpStatus.OK);
+            
         } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe um fornecedor com esse código"),HttpStatus.NOT_FOUND);
+            
+            //Erro de fornecedor não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um fornecedor com este código"),
+                    HttpStatus.NOT_FOUND);
+            
+        } catch (Exception e) {
+                    
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
     }
-    @DeleteMapping("fornecedors/{id}")
+    
+    @PutMapping("fornecedores/{id}")
+    public ResponseEntity update(@PathVariable("id") Integer id, 
+            @RequestBody FornecedorEntity fornecedor) {
+        try {
+            //Verifica se existe um fornecedor com o ID passado por parametro
+            FornecedorEntity found = this.service.findById(id);
+            
+            //Força que o novo objeto tenha o memso ID do objeto localizado
+            fornecedor.setId(found.getId());
+            
+            //Salvara o novo objeto no banco
+            this.service.save(fornecedor);            
+            
+            //Retorna o objeto que foi atualizado
+            return new ResponseEntity(fornecedor, HttpStatus.OK);
+            
+        } catch (ResourceNotFoundException e) {
+            
+            //Erro de fornecedor não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um fornecedor com este código"),
+                    HttpStatus.NOT_FOUND);
+            
+        } catch (Exception e) {
+                        
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @DeleteMapping("fornecedores/{id}")
     public ResponseEntity delete(@PathVariable("id") Integer id) {
         try {
+            //Verifica se existe um fornecedor com o ID passado por parametro
             FornecedorEntity found = this.service.findById(id);
-
+            
+            
+            //Exclui o item localizado
             this.service.delete(found.getId());
+            
+            //Como não há o que retornar, retorna-se apenas um status de "Sem Conteúdo"
             return new ResponseEntity(HttpStatus.NO_CONTENT);
+            
         } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe um fornecedor com esse código"),HttpStatus.NOT_FOUND);
+            
+            //Erro de fornecedor não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um fornecedor com este código"),
+                    HttpStatus.NOT_FOUND);
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+                    
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
 }
--- tas-server/src/main/java/br/edu/materdei/tas/server/controller/PedidoController.java
+++ tas-server/src/main/java/br/edu/materdei/tas/server/controller/PedidoController.java
@@ -4,74 +4,145 @@ import br.edu.materdei.tas.core.exception.ResourceNotFoundException;
 import br.edu.materdei.tas.server.utils.CustomErrorResponse;
 import br.edu.materdei.tas.venda.entity.PedidoEntity;
 import br.edu.materdei.tas.venda.service.PedidoService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@RestController
 public class PedidoController {
+    
     @Autowired
     private PedidoService service;
-
+    
     @GetMapping("pedidos")
     public ResponseEntity<List<PedidoEntity>> findAll() {
-        List<PedidoEntity> pedidos = this.service.findAll();
-
-        return new ResponseEntity(pedidos, HttpStatus.OK);
-    }
-    @GetMapping("pedidos/{id}")
-    public ResponseEntity findById(@PathVariable("id") Integer id) {
         try {
-            PedidoEntity pedido = this.service.findById(id);
-            return new ResponseEntity(pedido, HttpStatus.OK);
-        } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe um pedido com esse código"),HttpStatus.NOT_FOUND);
+            //Busca TODOS os registros no banco de dados
+            List<PedidoEntity> pedidos = service.findAll();
+
+            //Retorna a lista de pedidos
+            return new ResponseEntity(pedidos, HttpStatus.OK);  
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+            
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    
     @PostMapping("pedidos")
     public ResponseEntity create(@RequestBody PedidoEntity pedido) {
         try {
-
+            //Insere o pedido no bando de dados
             this.service.save(pedido);
+            
+            //Retorna o pedido inserido
             return new ResponseEntity(pedido, HttpStatus.CREATED);
-
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+            
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    @PutMapping("pedidos/{id}")
-    public ResponseEntity update(@PathVariable("ID") Integer id, @RequestBody PedidoEntity pedido) {
+    
+    @GetMapping("pedidos/{id}")
+    public ResponseEntity findByID(@PathVariable("id") Integer id) {
         try {
-            PedidoEntity found = this.service.findById(id);
-            pedido.setId(found.getId());
-
-            this.service.save(pedido);
-
+            
+            //Verifica se existe um pedido com o ID passado por parametro
+            PedidoEntity pedido = this.service.findById(id);
+            
+            //Retorna o pedido com o ID do parametro
             return new ResponseEntity(pedido, HttpStatus.OK);
+            
         } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe um pedido com esse código"),HttpStatus.NOT_FOUND);
+            
+            //Erro de pedido não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um pedido com este código"),
+                    HttpStatus.NOT_FOUND);
+            
+        } catch (Exception e) {
+                    
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
     }
+    
+    @PutMapping("pedidos/{id}")
+    public ResponseEntity update(@PathVariable("id") Integer id, 
+            @RequestBody PedidoEntity pedido) {
+        try {
+            //Verifica se existe um pedido com o ID passado por parametro
+            PedidoEntity found = this.service.findById(id);
+            
+            //Força que o novo objeto tenha o memso ID do objeto localizado
+            pedido.setId(found.getId());
+            
+            //Salvara o novo objeto no banco
+            this.service.save(pedido);            
+            
+            //Retorna o objeto que foi atualizado
+            return new ResponseEntity(pedido, HttpStatus.OK);
+            
+        } catch (ResourceNotFoundException e) {
+            
+            //Erro de pedido não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um pedido com este código"),
+                    HttpStatus.NOT_FOUND);
+            
+        } catch (Exception e) {
+                        
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
     @DeleteMapping("pedidos/{id}")
     public ResponseEntity delete(@PathVariable("id") Integer id) {
         try {
+            //Verifica se existe um pedido com o ID passado por parametro
             PedidoEntity found = this.service.findById(id);
-
+            
+            
+            //Exclui o item localizado
             this.service.delete(found.getId());
+            
+            //Como não há o que retornar, retorna-se apenas um status de "Sem Conteúdo"
             return new ResponseEntity(HttpStatus.NO_CONTENT);
+            
         } catch (ResourceNotFoundException e) {
-            return new ResponseEntity(new CustomErrorResponse("Não existe um pedido com esse código"),HttpStatus.NOT_FOUND);
+            
+            //Erro de pedido não encontrado
+            return new ResponseEntity(
+                    new CustomErrorResponse("Não existe um pedido com este código"),
+                    HttpStatus.NOT_FOUND);
+            
         } catch (Exception e) {
-            return new ResponseEntity(new CustomErrorResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+                    
+            //Qualquer outro erro
+            return new ResponseEntity(
+                    new CustomErrorResponse(e.getMessage()), 
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
 }
